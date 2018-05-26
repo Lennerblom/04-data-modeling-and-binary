82fcdede2383968cb3d16fc5ca22bc2b9b5edb4f
Phillip Kim <phildkim@gmail.com>
2018-05-26 14:36:38 -0700

    one more test

--- __test__/file.spec.js
+++ __test__/file.spec.js
@@ -1,28 +1,48 @@
 'use strict';
 
 const file = require('../src/lib/file.js');
+const parser = require('../src/lib/bitmap.js');
 
+//testing readFile
 describe('file.js', () => {
 
-  //let expect, actual;
-
-  it('should return error if invalid file path.', (done) => {
+  xit('should return error if invalid file path.', (done) => {
 
     file.readFile('missing.txt', (err) => {
       expect(err).not.toBeNull();
-  
       done();
     });
   });
 
-  it('should return file path', (done) => {
-  
+  xit('should return file path', (done) => {
+
     file.readFile(__dirname + '/../assets/bitmap.bmp', (err, data) => {
-      //expect(__dirname + '/../assets/bitmap.bmp').toBe('');
       expect(err).toBeNull();
-      
       expect(data).toBeInstanceOf(Buffer);
       done();
     });
   });
+});
+
+//testing bitmap parsing
+describe('bitmap.js', () => {
+  it('should grab the "type" data from original buffer', (done) => {
+    file.readFile(__dirname + '/../assets/bitmap.bmp', (err, data) => {
+      let expected = 'BM';
+      let actual = data.toString('utf-8', 0, 2);
+      console.log(actual);
+      expect(actual).toBe(expected);
+      done();
+    });
+  });
+
+  // it('should make new buffer that we can access', (done) => {
+  //   file.readFile(__dirname + '/../assets/bitmap.bmp', (err, data) => {
+  //     let expected = ;
+  //     let actual = parser(data);
+  //     console.log(actual);
+  //     expect(actual).toBe(expected);
+  //     done();
+  //   });
+  // })
 });
--- src/lib/bitmap.js
+++ src/lib/bitmap.js
@@ -1,33 +1,30 @@
 'use strict';
 
-module.exports = function(buffer) 
+module.exports = function (buffer) {
 
-const fs = require('fs');
+  const parsedBitmap = {};
 
-const buffer = fs.readFile(path,)
+  // Identify the offsets by reading the bitmap docs
+  const FILE_SIZE_OFFSET = 2;
+  const WIDTH_OFFSET = 18;
+  const HEIGHT_OFFSET = 22;
+  const NUM_COLORS_OFFSET = 46;
+  const COLOR_TABLE_OFFSET = 54;
+  const BYTES_PER_PIXEL_OFFSET = 28;
 
-const parsedBitmap = {};
+  //------------------------------------------------------
+  // READING INFORMATION FROM THE BITMAP FILE
+  //------------------------------------------------------
+  parsedBitmap.type = buffer.toString('utf-8', 0, 2);
+  parsedBitmap.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
+  parsedBitmap.bytesPerPixel = buffer.readInt16LE(BYTES_PER_PIXEL_OFFSET);
+  parsedBitmap.height = buffer.readInt32LE(HEIGHT_OFFSET);
+  parsedBitmap.width = buffer.readInt32LE(WIDTH_OFFSET);
+  parsedBitmap.numColors = buffer.readInt32LE(NUM_COLORS_OFFSET);
 
-// Identify the offsets by reading the bitmap docs
-const FILE_SIZE_OFFSET = 2;
-const WIDTH_OFFSET = 18;
-const HEIGHT_OFFSET = 22;
-const NUM_COLORS_OFFSET = 46;
-const COLOR_TABLE_OFFSET = 54;
-const BYTES_PER_PIXEL_OFFSET = 28;
+  // The size of the color table is calculated by finding the number of colors first and then multiplying by 4
+  let COLOR_TABLE_SIZE = parsedBitmap.numColors * 4;
+  parsedBitmap.colorTable = buffer.slice(COLOR_TABLE_OFFSET, COLOR_TABLE_SIZE);
 
-//------------------------------------------------------
-// READING INFORMATION FROM THE BITMAP FILE
-//------------------------------------------------------
-parsedBitmap.type = buffer.toString('utf-8', 0, 2);
-parsedBitmap.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
-parsedBitmap.bytesPerPixel = buffer.readInt16LE(BYTES_PER_PIXEL_OFFSET);
-parsedBitmap.height = buffer.readInt32LE(HEIGHT_OFFSET);
-parsedBitmap.width = buffer.readInt32LE(WIDTH_OFFSET);
-parsedBitmap.numColors = buffer.readInt32LE(NUM_COLORS_OFFSET);
-
-// The size of the color table is calculated by finding the number of colors first and then multiplying by 4
-let COLOR_TABLE_SIZE = parsedBitmap.numColors * 4;
-parsedBitmap.colorTable = buffer.slice(COLOR_TABLE_OFFSET,COLOR_TABLE_SIZE);
-
-console.log(parsedBitmap);
+  console.log(parsedBitmap);
+};
