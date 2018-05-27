36fc455023ad87c88992a68d173223f920b2f6ad
Phillip Kim <phildkim@gmail.com>
2018-05-26 17:55:22 -0700

    able to make new bmp file

--- __test__/file.spec.js
+++ __test__/file.spec.js
@@ -2,11 +2,12 @@
 
 const file = require('../src/lib/file.js');
 const parser = require('../src/lib/bitmap.js');
+const changer = require('../src/lib/transform.js');
 
 //testing readFile
 describe('file.js', () => {
 
-  it('should return error if invalid file path.', (done) => {
+  xit('should return error if invalid file path.', (done) => {
 
     file.readFile('missing.txt', (err) => {
       expect(err).not.toBeNull();
@@ -14,7 +15,7 @@ describe('file.js', () => {
     });
   });
 
-  it('should return file path', (done) => {
+  xit('should return file path', (done) => {
     file.readFile(__dirname + '/../assets/bitmap.bmp', (err, data) => {
       expect(err).toBeNull();
       expect(data).toBeInstanceOf(Buffer);
@@ -34,4 +35,26 @@ describe('bitmap.js', () => {
       done();
     });
   });
+
+  //write testing
+  describe('file.js', () => {
+    it('should create a new bitmap image with same values as the original', (done) => {
+      var buffer;
+      var original = __dirname + '/../assets/bitmap.bmp';
+      var newPath = __dirname + '/../assets/newby.bmp';
+
+      file.readFile(original, (err, data) => {
+        buffer = data;
+
+        file.writeFile(newPath, buffer, (err) => {
+          // let actual = parser(data);
+          expect(data).toBe(buffer);
+        });
+        done();
+      });
+    });
+  });
 });
+//readFile
+//apply readFile to variable, var obj
+//writeFile(filepath, obj, randomizer)
